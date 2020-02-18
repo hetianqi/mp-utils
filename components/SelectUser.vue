575c33bc138c71310fe287276c3373e66bc398f5
mp-it <mpit@mail.maipu.com\MPit@0717>
2020-02-18 16:32:24 +0800

    添加选人控件

---
+++ components/SelectUser.vue
@@ -0,0 +1,75 @@
+<template>
+	<el-select v-model="resultData"
+			   :multiple="multiple"
+			   :multiple-limit="multipleLimit"
+			   filterable
+			   remote
+			   clearable
+			   value-key="Badge"
+			   @change="resultChange"
+			   reserve-keyword
+			   placeholder="请输入姓名关键字或完整工号"
+			   :remote-method="remoteMethod"
+			   :loading="loading">
+		<el-option v-for="item in options"
+				   :key="item.Badge"
+				   :label="item.Name"
+				   :value="item">
+			<span style="float: left">{{ item.Name }}</span>
+			<span style="float: right; color: #8492a6; font-size: 13px">{{ item.OneDepName }}</span>
+		</el-option>
+	</el-select>
+</template>
+
+<script>
+	import axios from 'axios';
+	export default {
+		name: 'select-user',
+		props: {
+			multiple: {
+				type: Boolean,
+				default:false
+			},
+			multipleLimit: {
+				type: Number,
+				default:0
+			},
+		},
+		data() {
+			return {
+				options: [],
+				loading: false,
+				keyword:'',
+				resultData: [],
+				url: window.OP_CONFIG.selectorUrl,
+			}
+		},
+		created() {
+
+		},
+		methods: {
+			remoteMethod(query) {
+				if (query !== '') {
+					this.loading = true;
+					axios.jsonp(this.url+'/Organization/GetPeopleResult?depID=&jobID=&keyword=' + query).then(data => {
+						this.loading = false;
+						this.options = data.data;
+					})					
+				} else {
+					this.options = [];
+				}
+			},
+			resultChange() {
+				this.$emit("GetResultFn", this.resultData)
+				//if (this.multiple) {
+				//	this.$emit("GetResultFn", this.resultData)
+				//} else {
+				//	let resultData = this.options.filter(item => { item.Badge == this.resultData });
+				//	this.$emit("GetResultFn", resultData)
+				//}
+				
+			}
+		}
+
+	};
+</script>
